2ed51aefa6b0a5abd03a19de4b2d21098694718e
saakethkoka <saaketh.koka@gmail.com>
2021-04-07 23:35:59 -0500

    Deleting Calculate height function and adding files to .gitignore

--- DSAVLTreeNode.h
+++ DSAVLTreeNode.h
@@ -28,17 +28,6 @@ DSAVLTreeNode<A, T>::DSAVLTreeNode(A key, T value, DSAVLTreeNode *left, DSAVLTre
     this->height = 0;
 }
 
-//TODO do we need this?
-template<class A, class T>
-void DSAVLTreeNode<A, T>::calc_height() { //This calulates height
-    if(this->left == nullptr && this->right == nullptr){
-        height = 0;
-    }
-    this->left->calc_height();
-    this->right->calc_height();
-    height = std::max(this->left->height, this->right->height) + 1;
-}
-
 
 
 
